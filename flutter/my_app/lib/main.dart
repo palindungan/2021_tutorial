d09bf5c7b7251a093e596291f7cefc7645745918
palindungan <rizkika.palindungan1@gmail.com>
2021-03-15 15:07:04 +0700

    Update main.dart

--- flutter/my_app/lib/main.dart
+++ flutter/my_app/lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart'; // package dasar yg dibutuhkan untuk mengeksekusi program
 
+// entry point (dieksekusi ketika program pertama kali berjalan)
 void main() {
-  // entry point (dieksekusi ketika program pertama kali berjalan)
   runApp(MyApp());
 }
 
+// widget yg tidak memiliki state, tidak berubah dengan interaksi program
 class MyApp extends StatelessWidget {
-  // widget yg tidak memiliki state, tidak berubah dengan interaksi program
   // This widget is the root of your application.
+  // membangun widget (widget Tree)
   @override
   Widget build(BuildContext context) {
-    // membangun widget (widget Tree)
+    // setup aplikasi
     return MaterialApp(
-      // setup aplikasi
       title: 'Tims App',
       theme: ThemeData(
         primarySwatch: Colors.blue, // warna utama
@@ -29,16 +29,46 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Hello World"),
       ),
-      body: Column(
-        children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
-      ),
-    ); // set up basic structure of page
+      body: TextInputWidget(),
+    );
   }
 }
 
-class TestWidget extends StatelessWidget {
+// class for handle constructor
+class TextInputWidget extends StatefulWidget {
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+// class for handle state + rendering
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose(){
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text){
+    setState(() {
+      this.text = text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Hello ");
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: this.controller,
+          decoration: InputDecoration(
+              prefixIcon: Icon(Icons.message), labelText: "Type a Message"),
+        onChanged: (text) => this.changeText(text),
+        ),
+        Text(this.text)
+      ],
+    );
   }
 }
