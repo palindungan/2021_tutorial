7358bf87f36763a20f3b38bd664884f886d5e818
palindungan <rizkika.palindungan1@gmail.com>
2021-03-18 11:07:42 +0700

    Update detail.dart

--- flutter/food_app_course/lib/scr/screens/detail.dart
+++ flutter/food_app_course/lib/scr/screens/detail.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:food_app_course/scr/helpers/base_url.dart';
 import 'package:food_app_course/scr/helpers/style.dart';
 import 'package:food_app_course/scr/models/product.dart';
+import 'package:food_app_course/scr/widgets/custom_text.dart';
+import 'package:food_app_course/scr/widgets/small_button.dart';
 
 class Detail extends StatefulWidget {
   final Product product;
@@ -52,15 +54,75 @@ class _DetailState extends State<Detail> {
                             Navigator.pop(context);
                           }),
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          BaseUrl().imageAssetsUrl + "shopping-bag.png",
-                          width: 30,
-                          height: 30,
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Stack(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Stack(
+                                children: <Widget>[
+                                  Image.asset(
+                                    BaseUrl().imageAssetsUrl +
+                                        "shopping-bag.png",
+                                    width: 30,
+                                    height: 30,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Positioned(
+                              right: 5,
+                              bottom: 2,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: white,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: grey,
+                                          offset: Offset(2, 3),
+                                          blurRadius: 3)
+                                    ]),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 4.0, right: 4.0),
+                                  child: CustomText(
+                                    text: "2",
+                                    color: red,
+                                    size: 18,
+                                    weight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
                       )
                     ],
                   ),
+                  Positioned(
+                    right: 20,
+                    bottom: 55,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                                color: grey[400],
+                                offset: Offset(2, 1),
+                                blurRadius: 3)
+                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Icon(
+                          Icons.favorite,
+                          size: 22,
+                          color: red,
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             )
