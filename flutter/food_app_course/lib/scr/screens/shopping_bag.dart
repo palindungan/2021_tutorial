c9198d7fd1f90ee319afc3c02543c393b18c35ed
palindungan <rizkika.palindungan1@gmail.com>
2021-03-18 13:11:19 +0700

    Update shopping_bag.dart

--- flutter/food_app_course/lib/scr/screens/shopping_bag.dart
+++ flutter/food_app_course/lib/scr/screens/shopping_bag.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_app_course/scr/helpers/base_url.dart';
+import 'package:food_app_course/scr/helpers/style.dart';
+import 'package:food_app_course/scr/widgets/custom_text.dart';
 
 class ShoppingBag extends StatefulWidget {
   @override
@@ -10,8 +13,62 @@ class _ShoppingBagState extends State<ShoppingBag> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        // iconTheme: IconThemeData(color: black),
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: black,
+            ),
+            onPressed: () {}),
+        backgroundColor: white,
+        elevation: 0,
+        centerTitle: true,
+        title: CustomText(
+          text: "Shopping Bag",
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Image.asset(
+                    BaseUrl().imageAssetsUrl + "shopping-bag.png",
+                    width: 20,
+                    height: 20,
+                  ),
+                ),
+                Positioned(
+                  right: 7,
+                  bottom: 5,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              color: grey[400],
+                              offset: Offset(2, 3),
+                              blurRadius: 3)
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                      child: CustomText(
+                        text: "2",
+                        color: red,
+                        size: 16,
+                        weight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+      backgroundColor: white,
     );
   }
 }
