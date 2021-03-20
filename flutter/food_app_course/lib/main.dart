e3da263a9efef8d7403d8dc6504b1301289b6b08
palindungan <rizkika.palindungan1@gmail.com>
2021-03-20 07:13:35 +0700

    stuck

--- flutter/food_app_course/lib/main.dart
+++ flutter/food_app_course/lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app_course/scr/helpers/style.dart';
 import 'package:food_app_course/scr/providers/auth.dart';
-import 'file:///C:/xampp/htdocs/2021_tutorial/flutter/food_app_course/lib/scr/helpers/style.dart';
 import 'package:food_app_course/scr/screens/home.dart';
 import 'package:food_app_course/scr/screens/login.dart';
 import 'package:food_app_course/scr/widgets/loading.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider.value(value: AuthProvider.initialize())],
     child: MaterialApp(
@@ -16,22 +18,25 @@ void main() {
       theme: ThemeData(
         primarySwatch: red,
       ),
-      home: ScreenController(),
+      home: ScreensController(),
     ),
   ));
 }
 
-class ScreenController extends StatelessWidget {
+class ScreensController extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthProvider>(context);
     switch (auth.status) {
       case Status.Uninitialized:
+        print("error disini : Uninitialized");
         return Loading();
       case Status.Unauthenticated:
       case Status.Authenticating:
+      print("error disini : Authenticating");
         return LoginScreen();
       case Status.Authenticated:
+        print("error disini : Authenticated");
         return HomeScreen();
       default:
         return LoginScreen();
