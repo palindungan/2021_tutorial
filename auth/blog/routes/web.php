a18ca23c00af82da904003007d079716afcafbc1
palindungan <rizkika.palindungan1@gmail.com>
2021-06-10 11:39:03 +0700

    controller

--- auth/blog/routes/web.php
+++ auth/blog/routes/web.php
@@ -19,4 +19,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+});
